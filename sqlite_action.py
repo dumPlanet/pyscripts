5120f1130e24b3fbc7d0faf4d6e38258259fe21a
requiescat in pace <109029067+dumPlanet@users.noreply.github.com>
2023-06-03 22:06:53 +0200

    Update sqlite_action.py
    some code changes

--- sqlite_action.py
+++ sqlite_action.py
@@ -1,37 +1,41 @@
 import sqlite3
 import random
 
-# Verbindung zur Datenbank herstellen
+# connect database
 conn = sqlite3.connect('database/workdatabase.sqlite')
 cursor = conn.cursor()
 
 
-# Funktion zum Generieren einer eindeutigen zufälligen Nummer
+# generate random number function
 def generate_unique_number():
-    return random.randint(10000, 99999)  # Beispielbereich für die zufällige Nummer
+    return random.randint(10000, 99999)
 
 
-# SQL-Statement zum Abrufen der relevanten Datensätze
+def generateNumber(sql_select, sql_update, conn, cursor):
+    # load selected data from table
+    cursor.execute(sql_select)
+    rows = cursor.fetchall()
+
+    # loop
+    for row in rows:
+        author_name = row[0]
+        author_surname = row[1]
+
+        # generate a number for ...
+        authorID = generate_unique_number()
+
+        # execute update
+        cursor.execute(sql_update, (authorID, author_name, author_surname))
+        conn.commit()
+
+    # Verbindung zur Datenbank schließen
+    conn.close()
+
+
+# Variables
 sql_select = "SELECT author_name, author_surname FROM quotes"
+sql_update = "UPDATE quotes SET authorID = ? WHERE author_name = ? AND author_surname = ?"
 
-# Datensätze abrufen
-cursor.execute(sql_select)
-rows = cursor.fetchall()
 
-# Schleife über die abgerufenen Datensätze
-for row in rows:
-    author_name = row[0]
-    author_surname = row[1]
-
-    # Eindeutige Nummer generieren
-    authorID = generate_unique_number()
-
-    # SQL-Statement zum Aktualisieren des Datensatzes mit der eindeutigen Nummer
-    sql_update = "UPDATE quotes SET authorID = ? WHERE author_name = ? AND author_surname = ?"
-
-    # Datensatz aktualisieren
-    cursor.execute(sql_update, (authorID, author_name, author_surname))
-    conn.commit()
-
-# Verbindung zur Datenbank schließen
-conn.close()
+# Execute Script
+generateNumber(sql_select, sql_update, conn, cursor)
